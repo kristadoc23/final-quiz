9c653692fc1fc5cfc94e6be5ca8d40b0a96ab31d
kristadoc23 <123026546+kristadoc23@users.noreply.github.com>
2023-04-28 11:30:26 -0400

    Add files via upload

---
+++ Last Quiz/problem1.py
@@ -0,0 +1,18 @@
+import dbm
+new_file.open("words","c")
+line1= "today is friday\n"
+print(line1)
+line2= "spring semester is almost over\n"
+print(line2)
+line3 = "summer is almost here\n"
+print(line3)
+line4 = "dog days of summer\n"
+print(line4)
+line5= "no more school"
+print(line5)
+line6 = "I hope I get an A"
+print(line6)
+
+new_file.close()
+
+work.open("caption","c")
